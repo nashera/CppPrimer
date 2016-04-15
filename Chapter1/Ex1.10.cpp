33c7401275ea9db50a976aed864424fefab0d39f
nashera <zhangxi1014@gmail.com>
2016-04-15 22:57:17 +0800

    update

---
+++ Chapter1/Ex1.10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i = 10; i >= 0; i--){
+        cout << i << endl;
+    }
+    int j = 10;
+    while(j >= 0){
+        cout << j << endl;
+        j--;
+    }
+    return 0;
+    
+}
