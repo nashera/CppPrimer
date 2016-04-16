acc10110b8ea964c0e4bf5d80120d8fcdf0cc453
nashera <zhangxi1014@gmail.com>
2016-04-16 15:13:14 +0800

    update

---
+++ Chapter3/Ex3.11.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<vector<int> > ivec;
+//    vector<string> svec = ivec;
+    vector<string> svec(10, "null");
+    return 0;
+}
---
+++ Chapter3/Ex3.13.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    vector<int> ivec;
+    while(cin >> a){
+        ivec.push_back(a);
+    }
+    int sum = 0;
+    for(vector<int>::iterator iter = ivec.begin(); iter != ivec.end(); ++iter){
+        sum += *iter;
+    }
+    cout << sum << endl;
+    for (vector<int>::size_type ix = 0; ix != ivec.size(); ++ix){
+        if(ix % 2 == 0){
+            continue;
+        }
+        cout << ivec[ix-1] + ivec[ix] << endl;
+    }
+    
+
+    
+    return 0;
+}
