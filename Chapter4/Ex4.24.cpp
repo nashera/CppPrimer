0a143634e152eb8624a73228fed088fc145e6c86
zhangxi <zhangxi1014@gmail.com>
2016-04-17 22:10:46 +0800

    update

--- Chapter4/Ex4.24.cpp
+++ Chapter4/Ex4.24.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <cstdlib>
 using namespace std;
 int main(){
-    const char *str1 = "hello, world";
+    char str1[] = "hello, world";
     char *str2;
     char str3[40];
+        //  char *str3 = (char *)malloc(5);
     strcpy(str2, str1);
     strncpy(str3, str1, 3);
-    str3[5] = '\0';
-
+    str3[3] = '\0';
     cout << "str1 " << str1 << endl;
     cout << "str2 " << str2 << endl;
     cout << "str3 " << str3 << endl;
+//    free(str3);
     return 0;
     
     
