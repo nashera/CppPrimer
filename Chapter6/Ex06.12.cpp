2126bba2e68f1933245ba0347e1552a485bba7b0
zhangxi_picb <zhangxi1014@gmail.com>
2016-05-23 19:32:09 +0800

    update

--- Chapter6/Ex06.12.cpp
+++ Chapter6/Ex06.12.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #inclue <cstdio>
+#include <string>
+#include <map>
+
+
+map<string, int> stringMap;
+stringMap
+
+
 using namespace std;
 int main(){
+  string tmp, inp;
+  while(cin >> inp){
+	if(tmp.empty()){
+	  tmp = inp;
+	  next;
+	}
+	if(tmp == inp){
+	  
+	}
+}
   
 }
