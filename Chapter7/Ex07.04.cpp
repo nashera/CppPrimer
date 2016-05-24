54fb086f2c96f4a5b57e9c0eda95c021ea3aab0d
zhangxi_picb <zhangxi1014@gmail.com>
2016-05-24 13:29:33 +0800

    update

--- Chapter7/Ex07.04.cpp
+++ Chapter7/Ex07.04.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
+int main_test(int argc, char** argv){
+  cout << argv[1] << " " <<argv[2] << endl;
+  return 0;
+}
 int abs(int a){
   return (a>0 ? a : (-a));
 }
 
-int main(){
+int main(int argc, char** argv){
   int a = -8;
   cout << "abs " << abs(a) << endl;
-  return 0;
+  return main_test(argc, argv);
 }
